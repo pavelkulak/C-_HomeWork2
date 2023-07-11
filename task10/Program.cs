96961d9d1060fdb509b25243f8420df0351c6fe1
pavelkulak <kulak-pavel@yandex.ru>
2023-07-11 19:42:41 +0300

    begining

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine("Введите число - ");
+int number = int.Parse(Console.ReadLine());
+number = number % 100 / 10;
+Console.WriteLine("-> "+ number);
